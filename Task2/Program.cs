ebfa79bbc8aaf6c41f93d28247a5a9d1d023d00d
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-11-29 20:50:43 +0600

    Add solution of task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+ 452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+int countDigit = 0;
+int temp = number;//временная переменная для хранения числа
+int sum = 0;
+while (temp > 0)
+{
+    countDigit++;
+    temp/=10;
+}
+
+int[] numbers = new int[countDigit];
+int index = 0;
+while (number > 0)
+{
+    numbers[index] = number % 10;
+    number /=10;
+    index++;
+}
+for(int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine(sum);
